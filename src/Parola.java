5f90d94cbf305794c7e05dff4407ce3e320da914
Mihnea <mihneapaul8@gmail.com>
2017-04-04 15:19:03 +0300

    Initial commit

---
+++ src/Parola.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Mihnea on 02.04.2017.
+ */
+import java.util.Scanner;
+public class Parola {
+
+        // db
+        String user="ion";
+        String parola="fgf";
+
+        public static void main(String[] args) {
+            System.out.println("Hello World!");
+
+            /// in  while sa imi ceara usrr si parola
+            // daca usercerut = user & &  parolaceruta = parola
+            // lanseaza notepad
+
+            boolean nimerit = false;
+            while(!nimerit){
+                // intai definesc
+                // usecerut si parola cerut si
+                // citesc de la  tastatura valorile pt usercerut si parolaceruta
+//                nimerit = isCombinatiaBuna(usercerut, parolacerut);
+                if(nimerit) {
+                    lansez();
+                }
+            }
+
+        }
+
+        public static void lansez()
+        {
+            // voi cauta acasa cum lansez notepad sau orice alt program
+            // (e la fel pt orice program) din java
+        }
+
+
+//        public static boolean isCombinatiaBuna(String u, String p )
+//        {
+            // return true daca is alea din db altfel false
+//        }
+
+    }
+
